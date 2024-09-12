069e536d13bc083c5e8dd86fd079a992ac926dd0
sajna <sajnashuhaib38@gmail.com>
2024-09-12 12:35:16 +0530

    4th questio

---
+++ 7.between.dart
@@ -0,0 +1,8 @@
+void main(List<String> args) {
+  for (int i = 55; i <= 355; i = i + 2) {
+    // if (i == 10) {
+    //   continue;
+    // }
+    print(i);
+  }
+}
---
+++ 8.dart
@@ -0,0 +1,38 @@
+void main(List<String> args) {
+  List<int> a = [1, -5, -9, 5, 3];
+  for (int i = 0; i < a.length; i++) {
+    if (a[i] < 0) {
+      print(a[i]);
+    }
+  }
+  print("\n");
+  for (int i = 0; i < a.length; i++) {
+    if (a[i] > 0) {
+      print(a[i]);
+    }
+  }
+}
+
+
+
+
+
+
+
+
+
+// void main(List<String> args) {
+//   List<int> a = [23, 4, -6, 23, -9, 21, 3, -45, -8];
+
+//   for (int i = 0; i < a.length; i++) {
+//     if (a[i] < 0) {
+//       print(a[i]);
+//     }
+//   }
+//   print("\n");
+//   for (int i = 0; i < a.length; i++) {
+//     if (a[i] > 0) {
+//       print(a[i]);
+//     }
+//   }
+// }
