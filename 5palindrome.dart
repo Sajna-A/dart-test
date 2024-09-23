40698794e82705a11607012eb3667ce77312afde
sajna <sajnashuhaib38@gmail.com>
2024-09-23 10:16:25 +0530

    palindrome commit

--- 5palindrome.dart
+++ 5palindrome.dart
@@ -1,5 +1,4 @@
 void main(List<String> args) {
   String a = "MOM";
-
-  print(a.split('').reversed.join(''));
+  print(a.split('').reversed.join());
 }
--- sample.dart
+++ sample.dart
@@ -4,11 +4,24 @@ void main() {
   // String name = "Sajna";
   // int age = 30;
   // print("My name is ${name} \n age is ${age} ");
-  int row = 5;
-  for (int i = 1; i <= row; i++) {
-    for (int j = i; j <= row; j++) {
-      stdout.write("*");
-    }
-    stdout.write("\n");
-  }
+  // ================================================
+  // int row = 5;
+  // for (int i = 1; i <= row; i++) {
+  //   for (int j = i; j <= row; j++) {
+  //     stdout.write("*");
+  //   }
+  //   stdout.write("\n");
+  // }
+  // ================================================
+  // int n = 5;
+  // for (int i = 1; i <= n; i++) {
+  //   for (int j = 1; j <= i; j++) {
+  //     stdout.write(j);
+  //   }
+  //   stdout.write("\n");
+  // }
+  // ================================================
+  // String a = "MOM";
+  // print(a.split('').reversed.join());
+  // ================================================
 }
